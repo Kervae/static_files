5948e83a6fd440cf8fd9f729dc8a7621c3c505af
bot <bot6324@outlook.sk>
2021-06-06 00:49:14 +0800

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -590,7 +590,8 @@ $('.ssr-link,.ssr-qr,.ssr-config').click(function () {
                         });
                         $('#ssrinfomoal').modal();
                     } else {
-                        $('#ssrInfo').html('<pre style="color:#e83e8c">'+JSON.stringify(ssrNode['config'][index])+'</pre>');
+                        //$('#ssrInfo').html('<pre style="color:#e83e8c">'+JSON.stringify(ssrNode['config'][index])+'</pre>');
+						$('#ssrInfo').jsonViewer(ssrNode['config'][index]);
                         $('#ssrinfomoal').modal();
                     }
                 }else{
